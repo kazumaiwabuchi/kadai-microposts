e66dd5e60144b1c4b1b726e62b29a5303be667e4
kazuma.iwabuchi <amuzak4342mittuan@gmail.com>
2021-05-14 06:32:56 +0000

    modify UserModel

--- app/User.php
+++ app/User.php
@@ -41,13 +41,90 @@ class User extends Authenticatable
      */
     public function microposts()
     {
-        return $this->hasMany(Micropost::class);
+        return $this->hasMany(Micropost::class);//ユーザが多数のMicropostを所有する
     }
     /**
      * このユーザに関係するモデルの件数をロードする。
      */
-    public function loadRelationshipCounts()
+    public function loadRelationshipCounts()//ユーザが所有するmicropostの件数を取得
     {
         $this->loadCount('microposts');
     }
+    
+    /**
+     * このユーザがフォロー中のユーザ。（ Userモデルとの関係を定義）
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+        //第一引数：得られるModelクラス,第二引数：中間テーブル名,第三引数：中間テーブルに保存されている自分のidを示すカラム名,第四引数：関係先のidを示すカラム名
+    
+    }
+
+    /**
+     * このユーザをフォロー中のユーザ。（ Userモデルとの関係を定義）
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+     /**
+     * $userIdで指定されたユーザをフォローする。
+     *
+     * @param  int  $userId
+     * @return bool
+     */
+    public function follow($userId)
+    {
+        // すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 対象が自分自身かどうかの確認
+        $its_me = $this->id == $userId;
+
+        if ($exist || $its_me) {
+            // すでにフォローしているor対象が自分自身なら何もしない
+            return false;
+        } else {
+            // 未フォロー且つ自分自身ではないならフォローする
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    
+    /**
+     * $userIdで指定されたユーザをアンフォローする。
+     *
+     * @param  int  $userId
+     * @return bool
+     */
+    public function unfollow($userId)//フォローの削除
+    {
+        // すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 対象が自分自身かどうかの確認
+        $its_me = $this->id == $userId;
+
+        if ($exist && !$its_me) {
+            // すでにフォローしている且つ自分自身ではないなら、フォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            // 未フォローor自分自身なら実行しない
+            return false;
+        }
+    }
+    
+     /**
+     * 指定された $userIdのユーザをこのユーザがフォロー中であるか調べる。フォロー中ならtrueを返す。
+     *
+     * @param  int  $userId
+     * @return bool
+     */
+    public function is_following($userId)
+    {
+        // フォロー中ユーザの中に $userIdのものが存在するか
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
+
 }
