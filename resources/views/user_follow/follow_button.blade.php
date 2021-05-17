10f430b82ad43b4c867c2d10d6803f66578e42d0
kazuma.iwabuchi <amuzak4342mittuan@gmail.com>
2021-05-17 02:15:02 +0000

    modify follow_button

--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -1,10 +1,13 @@
-@if (Auth::id() != $user->id) //認証済みユーザ（閲覧者）が自分自身ではないとき、ボタンを表示する
-    @if (Auth::user()->is_following($user->id))//当該ユーザのidが閲覧者のis_following()内にあれば（フォロー済みなら）、アンフォローボタン表示
+{{--認証済みユーザ（閲覧者）が自分自身ではないとき、ボタンを表示する--}}
+@if (Auth::id() != $user->id)
+    {{--当該ユーザのidが閲覧者のis_following()内にあれば（フォロー済みなら）、アンフォローボタン表示--}}
+    @if (Auth::user()->is_following($user->id))
         {{-- アンフォローボタンのフォーム --}}
         {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfollow', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
-    @else//未フォローなら、フォローボタン表示
+    {{--未フォローなら、フォローボタン表示--}}
+    @else
         {{-- フォローボタンのフォーム --}}
         {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
             {!! Form::submit('Follow', ['class' => "btn btn-primary btn-block"]) !!}
