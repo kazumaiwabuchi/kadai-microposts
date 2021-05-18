baf008698ef279c9a178d18f4a62633c90fda7fa
kazuma.iwabuchi <amuzak4342mittuan@gmail.com>
2021-05-18 06:40:45 +0000

    create favorites_view and modify navtab

---
+++ resources/views/users/favorites.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app'){{--共通レイアウト継承--}}
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            {{-- ユーザ情報 --}}
+            @include('users.card')
+        </aside>
+        <div class="col-sm-8">
+            {{-- タブ --}}
+            @include('users.navtabs'){{--ナビタブ--}}
+            {{-- お気に入り投稿一覧 --}}
+            @include('microposts.microposts'){{--ユーザ一覧ページの継承,フォロー中ユーザ一覧が渡される--}}
+        </div>
+    </div>
+@endsection
